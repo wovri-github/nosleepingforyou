d23585ea17a28272f5d3861d657b3788d1241b3e
mm-102 <maciej.kowalski@student.put.poznan.pl>
2023-05-12 21:37:26 +0200

    bed timer depend on victim position & victim name under bar

--- Objects/bed.gd
+++ Objects/bed.gd
@@ -2,8 +2,9 @@ extends StaticBody3D
 
 const sleep_bar_tscn = preload("res://sleep_bar.tscn")
 const SLEEP_NEEDED = 30
-var someone_sleeping = true # true only for testing
+var someone_sleeping = false
 signal sleep_timer(time_left)
+signal sleeping_vicim_name(text)
 
 func change_color():
 	$MeshInstance3D.mesh.material.albedo_color = Color.RED
@@ -11,17 +12,36 @@ func change_color():
 func reset_color():
 	$MeshInstance3D.mesh.material.albedo_color = Color.AQUA
 
+func _on_pillow_area_entered(area):
+	if area.is_in_group("Victims"):
+		someone_sleeping = true
+		$Timer.paused = false
+		sleep_timer.emit(100)
+		$Timer.start(SLEEP_NEEDED)
+		sleeping_vicim_name.emit(area.name)
+
+func _on_pillow_area_exited(area):
+	if area.is_in_group("Victims"):
+		someone_sleeping = false
+		sleep_timer.emit(0)
+		$Timer.paused = true
+		sleeping_vicim_name.emit("")
+
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("Player"):
 		change_color()
-		$Timer.paused = true
-		sleep_timer.emit(100)
+		if someone_sleeping:
+			$Timer.paused = true
+			sleep_timer.emit(100)
+		return
 
 func _on_area_3d_body_exited(body):
 	if body.is_in_group("Player"):
 		reset_color()
-		$Timer.paused = false
-		$Timer.start(SLEEP_NEEDED)
+		if someone_sleeping:
+			$Timer.paused = false
+			$Timer.start(SLEEP_NEEDED)
+		return
 
 func _ready():
 	$MeshInstance3D.mesh.material = $MeshInstance3D.mesh.material.duplicate()
@@ -29,15 +49,18 @@ func _ready():
 	
 	var bar = sleep_bar_tscn.instantiate()
 	get_tree().root.get_node("Main/Bars").add_child(bar)
-	sleep_timer.connect(bar.set_value)
-	$Timer.start(SLEEP_NEEDED)
+	sleep_timer.connect(bar.set_bar_value)
+	sleeping_vicim_name.connect(bar.set_victim_name)
 
 func _process(delta):
-	if !$Timer.paused:
-#		print(100*$Timer.time_left/SLEEP_NEEDED)
+	if !$Timer.paused and someone_sleeping:
 		sleep_timer.emit(100*$Timer.time_left/SLEEP_NEEDED)
+		
 
 func _on_timer_timeout():
 	add_child(preload("res://GUI/GameOver.tscn").instantiate())
 	
+
+
+
 
---
+++ sleep_bar.gd
@@ -0,0 +1,8 @@
+extends VBoxContainer
+
+
+func set_bar_value(value):
+	$Bar.value = value
+
+func set_victim_name(text):
+	$Label.text = text
