a21fb12ac370afce8a820cb60d7d185b7ce605a8
mm-102 <maciej.kowalski@student.put.poznan.pl>
2023-05-12 20:26:56 +0200

    add prototype sleep bar

--- bed.gd
+++ bed.gd
@@ -1,17 +1,39 @@
 extends StaticBody3D
 
+const sleep_bar_tscn = preload("res://sleep_bar.tscn")
+const SLEEP_NEEDED = 10
+var someone_sleeping = true # true only for testing
+signal sleep_timer(time_left)
+
 func change_color():
 	$MeshInstance3D.mesh.material.albedo_color = Color.RED
 	
 func reset_color():
 	$MeshInstance3D.mesh.material.albedo_color = Color.AQUA
 
-
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("Player"):
 		change_color()
-
+		$Timer.paused = true
+		sleep_timer.emit(100)
 
 func _on_area_3d_body_exited(body):
 	if body.is_in_group("Player"):
 		reset_color()
+		$Timer.paused = false
+		$Timer.start(SLEEP_NEEDED)
+
+func _ready():
+	var bar = sleep_bar_tscn.instantiate()
+	get_tree().root.get_node("Main/Bars").add_child(bar)
+	sleep_timer.connect(bar.set_value)
+	$Timer.start(SLEEP_NEEDED)
+
+func _process(delta):
+	if !$Timer.paused:
+#		print(100*$Timer.time_left/SLEEP_NEEDED)
+		sleep_timer.emit(100*$Timer.time_left/SLEEP_NEEDED)
+
+func _on_timer_timeout():
+	print("game over")
+
