e6738d7dc309f5fb45c7c2c7c76ccad3b0ac6b2b
Adam Misiak <adammisiak2@gmail.com>
2023-05-13 08:22:15 +0200

    Add other places for victims to go

---
+++ Objects/place.gd
@@ -0,0 +1,26 @@
+extends Node3D
+class_name Place
+
+@export var time_consumption_min = 3
+@export var time_consumption_max = 6
+var is_occupied = false
+var victim : CharacterBody3D = null
+
+func victim_reached(body):
+	is_occupied = true
+	$Timer.paused = false
+	$Timer.stop()
+	randomize()
+	var time_consumption = randi_range(\
+			time_consumption_min, time_consumption_max)
+	$Timer.start(time_consumption)
+	victim = body
+
+func _on_timer_timeout():
+	print("Timer reached")
+	is_occupied = false
+	$Timer.stop()
+	victim.move_on()
+	victim = null
+	add_to_group("Unoccupied_places")
+	
--- Objects/victim.gd
+++ Objects/victim.gd
@@ -2,7 +2,9 @@ extends CharacterBody3D
 
 const WAKING_TIME = 5
 const SPEED = 5.0
+const BED_CHANCE = 0
 var sleeping = false
+var target_place
 var bed: Bed = null
 var rng = RandomNumberGenerator.new()
 @onready var nav_agent := $NavigationAgent3D
@@ -11,6 +13,8 @@ func _ready():
 	rng.randomize()
 	$Guy/AnimationPlayer.queue("Idle")
 
+	
+
 func wake_up():
 	$Guy/AnimationPlayer.stop()
 	$Guy/AnimationPlayer.clear_queue()
@@ -18,13 +22,23 @@ func wake_up():
 	$Timer.start(WAKING_TIME)
 
 func _on_timer_timeout():
-	var beds = get_tree().get_nodes_in_group("Unoccupied_beds")
+	move_on()
 
-	var target_bed = beds[rng.randi() % beds.size()]
-	bed = target_bed
-	target_bed.remove_from_group("Unoccupied_beds")
-	set_target_location(target_bed.position)
+func move_on():
+	randomize()
 	sleeping = false
+	if randf() <= BED_CHANCE:
+		var beds = get_tree().get_nodes_in_group("Unoccupied_beds")
+		var target_bed = beds[rng.randi() % beds.size()]
+		bed = target_bed
+		target_place = target_bed
+		target_bed.remove_from_group("Unoccupied_beds")
+		set_target_location(target_bed.position)
+	else:
+		var places = get_tree().get_nodes_in_group("Unoccupied_places")
+		target_place = places[rng.randi() % places.size()]
+		target_place.remove_from_group("Unoccupied_places")
+		set_target_location(target_place.position)
 	$Guy/AnimationPlayer.stop()
 	$Guy/AnimationPlayer.clear_queue()
 	$Guy/AnimationPlayer.queue("walk")
@@ -40,22 +54,14 @@ func _physics_process(_delta):
 		position = nav_agent.target_position + Vector3(-0.65,0,0)
 		position.y = 0.1
 		
-	if !sleeping:
+	if !nav_agent.is_target_reached():
 		var current_location = global_transform.origin
 		var next_location = nav_agent.get_next_path_position()
-		if nav_agent.distance_to_target() < 1.5:
-			sleeping = true
-			$Guy/AnimationPlayer.stop()
-			$Guy/AnimationPlayer.clear_queue()
-			$Guy/AnimationPlayer.queue("layDown")
-			$Guy/AnimationPlayer.queue("sleep")
-			rotation.y = bed.rotation.y - PI / 2
-		else:
-			var new_velocity = (next_location - current_location).normalized() * SPEED
-			new_velocity.y = 0
-			velocity = new_velocity
-			rotation.y = lerp_angle(rotation.y, Vector2(velocity.z, velocity.x).angle(), 0.1)
-			move_and_slide()
+		var new_velocity = (next_location - current_location).normalized() * SPEED
+		new_velocity.y = 0
+		velocity = new_velocity
+		rotation.y = lerp_angle(rotation.y, Vector2(velocity.z, velocity.x).angle(), 0.1)
+		move_and_slide()
 		
 
 func set_target_location(target_location):
@@ -65,3 +71,21 @@ func set_target_location(target_location):
 func _on_animation_player_animation_changed(old_name, new_name):
 	if old_name == "layDown" and new_name == "sleep":
 		bed.victim_sleep(self)
+
+
+func _on_navigation_agent_3d_target_reached():
+	if target_place is Bed:
+		sleeping = true
+		$Guy/AnimationPlayer.stop()
+		$Guy/AnimationPlayer.clear_queue()
+		$Guy/AnimationPlayer.queue("layDown")
+		$Guy/AnimationPlayer.queue("sleep")
+		rotation.y = bed.rotation.y - PI / 2
+	elif target_place is Place:
+		assert(!target_place.is_occupied, "This bed shouldnt be occupied")
+		$Guy/AnimationPlayer.stop()
+		$Guy/AnimationPlayer.clear_queue()
+		$Guy/AnimationPlayer.queue("Idle")
+		target_place.victim_reached(self)
+		
+	
