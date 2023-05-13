d085abd2505a76bffdde658b046b28927b207897
Adam Misiak <adammisiak2@gmail.com>
2023-05-13 11:45:39 +0200

    Quit button

--- GUI/Menu.gd
+++ GUI/Menu.gd
@@ -5,3 +5,7 @@ func _on_button_pressed():
 	get_tree().change_scene_to_packed(game)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	get_tree().paused = false
+
+
+func _on_button_2_pressed():
+	get_tree().quit()
