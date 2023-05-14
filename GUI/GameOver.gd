0dcc273a1992f92bc7534eb82d2d664ab57f5fb2
Venedie <pagasnikola@gmail.com>
2023-05-14 13:28:37 +0200

    Change in GameOver mechanic

--- GUI/GameOver.gd
+++ GUI/GameOver.gd
@@ -1,8 +1,24 @@
 extends Control
 
 const MAIN_MENU = "uid://bor178du6hymf"
+const game = preload("res://Map.tscn")
+const game_lvl_increase = 0.2
 var text = "NULL"
 @onready var reason_n = $Reason
+
+
+func setup(reason: int, lvl, wins):
+	if reason == Over.WIN:
+		$Label.hide()
+		$PlayAgain.show()
+	else:
+		$Label.show()
+		$PlayAgain.hide()
+	$Lvl/Num.set_text(str(lvl))
+	$Wins/Num.set_text(str(wins))
+	$PlayAgain/NextLvl/Num.set_text(str(lvl + game_lvl_increase))
+	
+	pass
 
 func set_reason_text(_text: String):
 	text = _text
@@ -13,4 +29,12 @@ func _ready():
 
 func _on_go_menu_pressed():
 	get_tree().change_scene_to_file(MAIN_MENU)
-	self.queue_free()
+	self.hide()
+
+
+func _on_play_again_pressed():
+	get_tree().change_scene_to_packed(game)
+	GameManager.set_dificulty(game_lvl_increase)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	get_tree().paused = false
+	self.hide()
--- GameManager.gd
+++ GameManager.gd
@@ -1,15 +1,28 @@
 extends Node
 
-var GAME_OVER = preload("res://GUI/GameOver.tscn")
+const GAMEOVER = preload("res://GUI/GameOver.tscn")
+var wins = 0
+var result = 0
+var lvl = 1
 var victim_speed = 1
 var sleep_devider = 1
 
+
 func set_dificulty(value):
+	lvl += value
 	victim_speed += value
-	sleep_devider += value *0.5
+	sleep_devider += value * 0.5
 
 func end_game(reason: int, text: String):
-	var game_over_inst = GAME_OVER.instantiate()
-	game_over_inst.set_reason_text(text)
-	add_child(game_over_inst)
+	var gameover_n = GAMEOVER.instantiate()
+	if reason == Over.WIN:
+		wins += 1
+		result = wins
+	else:
+		result = wins
+		wins = 0
+	gameover_n.show()
+	gameover_n.setup(reason, lvl, wins)
+	gameover_n.set_reason_text(text)
+	add_child(gameover_n)
 	get_tree().paused = true
